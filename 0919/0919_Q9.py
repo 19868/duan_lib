63578f61f5b6797ca73822bad7ce34e4497d2f85
19868 <144769059+19868@users.noreply.github.com>
2023-10-17 18:22:52 +0800

    Update 0919_Q9.py

--- 0919/0919_Q9.py
+++ 0919/0919_Q9.py
@@ -6,7 +6,7 @@ def func(times):
         x = random.uniform(2,3)
         y = random.uniform(0,45)
         d=x*x+4*x*math.sin(x)-y
-        if d<=0:
+        if d>=0:
             sum+=1
     return(45*sum)/(times)
 print(func(1000000))
